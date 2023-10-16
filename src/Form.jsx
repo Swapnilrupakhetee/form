e85e9a671f0ffdbcd6ae9da1e027ee10fa4fdadc
Swapnil <rupakheteeswapnil@gmail.com>
2023-10-16 20:56:33 +0545

    3rd commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Form.css"
 
 function Form() {
+    const [values, setValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+        
+
+    })
+    
+
+    
+    
+    
+    
 
   return (
     <div className='form-container'>
         <form className='form'>
-            <div className='submitted'>
-                Thank you for filling the form !!!</div>
+            <div className="submitted">
+                Thank you for filling the form !!!
+                </div>
             <div className='form-group'>
 
                 <label className='form-label'>Name: </label>
-                <input className='form-input' name='firstName' placeholder='Name' />
+                <input value={values.firstName} className='form-input' name='firstName' placeholder='Name' />
             </div>
             <div className='form-group'>
                 <label className='form-label'>Last Name: </label>
-                <input className='form-input' name='lastName' placeholder='Email' />
+                <input value={values.lastName} className='form-input' name='lastName' placeholder='Email' />
             </div>
             <div className='form-group'>
                 <label className='form-label'>Email: </label>
-                <input className='form-input' name='email' placeholder='Email' />
+                <input value={values.email} className='form-input' name='email' placeholder='Email' />
             </div>
             
             <div className='form-group-button'>
-                <button className='form-button'>Submit</button>
+                <button className='form-button' >Submit</button>
             </div>
         </form>
     </div>
